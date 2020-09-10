6178daa43acf606b27d0882079901a14e37d8394
jyye1992 <jyye1992@hotmail.com>
2020-09-10 09:46:28 +0800

    test: add same instance

---
+++ singleton/test/SingletonTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class SingletonTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function sameCall()
+    {
+        $firstCall = Singleton::getInstance();
+        $secondCall = Singleton::getInstance();
+
+        $this->assertInstanceOf(Singleton::class, $firstCall);
+        $this->assertSame($firstCall, $secondCall);
+    }
+
+
+}
